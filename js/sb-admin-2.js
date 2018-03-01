6ead68ed0078ac5f3c97307e46866cd4acf11315
Mariel Mcneil <marielmcneil78@gmail.com>
2018-03-01 01:07:59 -0500

    accounts can now be added to the chart of accounts table

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -2,6 +2,9 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
+
+
+
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
@@ -40,3 +43,7 @@ $(function() {
         }
     }
 });
+
+
+/***                                                                 CHART OF ACCOUNTS related scripts here                                                                *****/
+//add a row 
